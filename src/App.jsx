2a4b19cea8598817ae33edfd503f8048a78d473e
Raymundo Rabago <raymundo_rabago@outlook.com>
2023-09-17 17:47:50 -0700

    cambio de proyecto a pocketbase

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,13 @@
 import React, { Fragment, useCallback, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
-import { AuthContextProvider, useAuthState } from './firebase';
 
-import Home from './components/Home';
-
-// https://github.com/codingwithjustin/firebase-v9-auth-react
-// https://dev.to/jsbroks/firebase-authentication-with-react-and-guarded-routes-41nm
-
-const AuthenticatedRoute = ({ component: C, ...props }) => {
-  const { isAuthenticated } = useAuthState()
-  console.log(`AuthenticatedRoute: ${isAuthenticated}`)
-  return (
-    <Route
-      {...props}
-      render={routeProps =>
-        isAuthenticated ? <C {...routeProps} /> : <Redirect to="/login" />
-      }
-    />
-  )
-}
-
-const UnauthenticatedRoute = ({ component: C, ...props }) => {
-  const { isAuthenticated } = useAuthState()
-  console.log(`UnauthenticatedRoute: ${isAuthenticated}`)
-  return (
-    <Route
-      {...props}
-      render={routeProps =>
-        !isAuthenticated ? <C {...routeProps} /> : <Redirect to="/" />
-      }
-    />
-  )
-}
+import Home from './components/Admin';
 
 const App = () => {
   return (
-    <AuthContextProvider>
-      <Router>
-        <div>
-          <Link to="/">Home</Link> | <Link to="/login">Login</Link> |{' '}
-          <Link to="/signup">SignUp</Link>
-        </div>
-        <AuthenticatedRoute exact path="/" component={Home} />
-        <UnauthenticatedRoute exact path="/signup" component={SignUp} />
-        <UnauthenticatedRoute exact path="/login" component={Login} />
-      </Router>
-    </AuthContextProvider>
+
   )
 }
 
 export default App;
-
-/*
-  const App = () => {
-    const lenses = fetchData('https://sheet.best/api/sheets/c2af4951-3c2d-4cd1-a4da-cc630463207d');
-    return (
-      <Fragment>
-        <div className="soluxe-grid">
-          {lenses.map((data) => (
-            <div className="soluxe-item" key={data.id}>
-              <img src={`https://res.cloudinary.com/dradvgtkq/image/upload/c_thumb,w_120,g_face/v1677686608/eyewear/${data.img}`} alt={data.producto} className="w-100" />
-              <span>{data.costo}</span>
-            </div>
-          ))}
-        </div>
-      </Fragment>
-    );
-  };
-  export default App;
-  */
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,54 +1,11 @@
-import { useCallback } from 'react';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-
-import useAuthState from '../firebase';
-
-const AuthenticatedRoute = ({ component: C, ...props }) => {
-  const { isAuthenticated } = useAuthState()
-  console.log(`AuthenticatedRoute: ${isAuthenticated}`)
-  return (
-    <Route
-      {...props}
-      render={routeProps =>
-        isAuthenticated ? <C {...routeProps} /> : <Redirect to="/login" />
-      }
-    />
-  )
-}
-
-
-const UnauthenticatedRoute = ({ component: C, ...props }) => {
-  const { isAuthenticated } = useAuthState()
-  console.log(`UnauthenticatedRoute: ${isAuthenticated}`)
-  return (
-    <Route
-      {...props}
-      render={routeProps =>
-        !isAuthenticated ? <C {...routeProps} /> : <Redirect to="/" />
-      }
-    />
-  )
-}
 
 
 
 export const Login = () => {
-  const handleSubmit = useCallback(async e => {
-    e.preventDefault()
-
-    const { email, password } = e.target.elements
-    const auth = getAuth()
-    try {
-      await signInWithEmailAndPassword(auth, email.value, password.value)
-    } catch (e) {
-      alert(e.message)
-    }
-  }, [])
-
   return (
     <>
       <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
+      <form>
         <input name="email" placeholder="email" type="email" />
         <input name="password" placeholder="password" type="password" />
         <button type="submit">Login</button>
