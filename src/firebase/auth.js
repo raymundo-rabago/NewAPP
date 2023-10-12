97e1bc57d8ce0ae75e486b2dc14a8506f4bab5ad
Ray <raymundo_rabago@outlook.com>
2023-10-12 16:24:55 -0700

    config changes

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -17,21 +17,3 @@ export function logout () {
 export function login (email, pw) {
   return firebaseAuth().signInWithEmailAndPassword(email, pw)
 }
-
-signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-
-
-signOut(auth).then(() => {
-  // Sign-out successful.
-  }).catch((error) => {
-    // An error happened.
-  });
