21c782f735559c15447a2a9cc743530efad88806
RAY <raymundo.rabago@jastmedia.com>
2024-03-22 16:45:24 -0700

    login component update

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,21 @@
 
 import "./index.scss";
-import { useState, useEffect } from "react";
-import { createClient } from "@supabase/supabase-js";
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
+import React from 'react';
+import  { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
-export default function App() {
-
-  const [session, setSession] = useState(null);
-
-  const customTheme = {
-    default: {
-      colors: {
-        brand: 'hsl(153 60.0% 53.0%)',
-        brandAccent: 'hsl(154 54.8% 45.1%)',
-        brandButtonText: 'white',
-        // ..
-      },
-    },
-  }
+import Login from "./components/loginPage";
+import Admin from "./components/Admin";
 
 
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-    return () => subscription.unsubscribe();
-  }, []);
-  if (!session) {
-    return (
-      <div className="login-wrapper">
-        <div>
-          <Auth supabaseClient={supabase} theme="default" appearance={{ theme: customTheme }} />
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <div>Logged in!</div>
-        <button onClick={() => supabase.auth.signOut()}>Sign out</button>
-      </div>
-    );
-  }
+function App() {
+  return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login/>} />
+          <Route path="/admin" element={<Admin/>} />
+        </Routes>
+      </Router>
+  );
 }
+
+export default App;
