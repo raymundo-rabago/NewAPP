69b324a7fdb7fda71e8ef2e9b2e8dc738459eebd
RAY <raymundo.rabago@jastmedia.com>
2024-04-08 13:44:51 -0700

    auth0 changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,24 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom/client'
-// import { AuthProvider } from './AuthContext';
+import ReactDOM from 'react-dom/client';
+
+import { Auth0Provider } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Auth0ProviderNav } from "./Auth0ProviderNav";
+
 import App from './App.jsx';
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Auth0ProviderNav>
+        <App />
+      </Auth0ProviderNav>
+    </BrowserRouter>
+  </React.StrictMode>
 )
