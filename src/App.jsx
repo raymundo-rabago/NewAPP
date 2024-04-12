cd09fd2bf4c1e49f21781be4183ee2fc44d56ff3
RAY <raymundo.rabago@jastmedia.com>
2024-04-12 10:06:18 -0700

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,15 @@
-import '@mantine/core/styles.css';
-import { createTheme, MantineProvider } from '@mantine/core';
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import { AuthPage } from './components/AuthPage';
-
-const theme = createTheme({
-  fontFamily: 'system-ui, sans-serif',
-  defaultRadius: 0,
-  white: 'GhostWhite';
-  primaryColor: 'DarkGoldenRod',
-  headings: {
-    fontFamily: 'Didot, Bodoni MT, Noto Serif Display, URW Palladio L, P052, Sylfaen, serif';
-    fontWeight: '400';
-  };
-});
 
 export function App() {
   return (
-    <MantineProvider theme={theme}>
       <Router>
-        <Switch>
-          <Route exact path="/" component={AuthPage} />
-        </Switch>
+        <Routes>
+          <Route exact path="/" element={<AuthPage />} />
+          <Route exact path="/reset" element={<ResetPage />} />
+          <Route exact path="/admin" element={<Admin />} />
+        </Routes>
       </Router>
-    </MantineProvider>
   );
 };
