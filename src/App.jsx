01f47e3b616b28498bea110650bf093be4a0b785
RAY <raymundo.rabago@jastmedia.com>
2024-04-22 16:32:25 -0700

    geist ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import AuthPage from './components/AuthPage';
 import ResetPage from './components/ResetPage';
 import ResetPage from './components/AddProduct';
 import AdminPage from './components/AdminPage';
+
+
 
 
 export function App() {
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.jsx';
 
-import 'index.css';
+import { GeistProvider, CssBaseline } from '@geist-ui/core';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
+    <GeistProvider>
+      <CssBaseline />
       <App />
+    </GeistProvider>
   </React.StrictMode>
 );
