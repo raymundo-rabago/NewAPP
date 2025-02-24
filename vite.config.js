c21351be9a45a57d117c7c1742a5c51adae51c89
RAY <raymundo.rabago@jastmedia.com>
2025-02-24 15:51:18 -0700

    update packages modules

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { reactRouter } from "@react-router/dev/vite";
 import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+
  
 export default defineConfig({
   plugins: [
     react(),
+    reactRouter(),
     VitePWA({
       injectRegister: 'inline'
     })
